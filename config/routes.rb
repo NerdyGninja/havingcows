f507845cf53cc73fefc703b1b21c34ae7b1ed0b8
Jacques le Roux <nerdy.gninja@gmail.com>
2018-08-16 09:49:23 +0200

    add resposive iframe to css

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :songs
-	root 'simple_pages#index'
+	#root 'simple_pages#index'
   get 'simple_pages/index'
+  root 'simple_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
