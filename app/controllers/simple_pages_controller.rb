37cd820cc2428c4595e4d8be50a9a53321eeacac
Jacques le Roux <nerdy.gninja@gmail.com>
2018-08-17 13:35:29 +0200

    add merchandise scaffold

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -6,4 +6,11 @@ class SimplePagesController < ApplicationController
 		@featured_events = Event.limit(6)
 	end
 
+	def thank_you
+		@name = params[:name]
+		@email = params[:email]
+		@message = params[:message]
+		UserMailer.contact_form(@email, @name, @message).deliver_now
+	end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_17_093904) do
+ActiveRecord::Schema.define(version: 2018_08_17_112643) do
 
   create_table "events", force: :cascade do |t|
     t.string "event_name"
@@ -18,6 +18,16 @@ ActiveRecord::Schema.define(version: 2018_08_17_093904) do
     t.string "venue"
     t.integer "entrance"
     t.string "tickets_available_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "merchandises", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.text "description"
+    t.string "image_url"
+    t.string "colour"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
