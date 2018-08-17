837295b31a890c16cfba5f2917d1381b72d4d43f
Jacques le Roux <nerdy.gninja@gmail.com>
2018-08-17 11:44:32 +0200

    add events scaffold

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -3,6 +3,7 @@ class SimplePagesController < ApplicationController
 	def landing_page
 		@featured_songs = Song.limit(6)
 		@featured_videos = Video.limit(6)
+		@featured_events = Event.limit(6)
 	end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_17_083931) do
+ActiveRecord::Schema.define(version: 2018_08_17_093904) do
 
   create_table "events", force: :cascade do |t|
     t.string "event_name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2018_08_17_083931) do
     t.string "youtube_link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
