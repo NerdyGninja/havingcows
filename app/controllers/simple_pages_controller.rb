f0c6d5664d6af645d07e2a2c2b2e58339ac2ed8a
Jacques le Roux <nerdy.gninja@gmail.com>
2018-08-20 19:43:49 +0200

    add modal youtube links for videos

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -2,7 +2,7 @@ class SimplePagesController < ApplicationController
 
 	def landing_page
 		@featured_songs = Song.limit(6)
-		@featured_videos = Video.limit(6)
+		@featured_videos = Video.all
 		@featured_events = Event.limit(6)
 		@featured_merchandises = Merchandise.limit(4)
 	end
