7e0c80693c035fb299b08e4dd83423b7ad5cfb75
Jacques le Roux <nerdy.gninja@gmail.com>
2018-10-08 10:33:23 +0200

    add orders controller with views

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :events
   resources :videos
   resources :songs
+  resources :orders, only: [:index, :show, :create, :destroy]
 
   root 'simple_pages#landing_page'
   get 'simple_pages/landing_page'
